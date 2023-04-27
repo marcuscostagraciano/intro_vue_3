89ddb940924edce2e5583304a25a1be93cb5866d
Marcus Vinicius da Costa Graciano <48135737+marcuscostagraciano@users.noreply.github.com>
2023-04-27 09:48:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-const product = 'Socks'
+// const app = Vue.createApp( {
+//     data () {
+//         return {
+//             product: 'Socks'
+//         }
+//     }
+// })
+
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Boots',  //valor atualizado
+            description: 'some nice leather boots'
+        }
+    }
+})
