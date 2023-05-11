0097375b436c3efbc0568bc5937d878d99d2430d
Marcus Vinicius da Costa Graciano <48135737+marcuscostagraciano@users.noreply.github.com>
2023-05-11 09:18:01 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -11,5 +11,18 @@ const app = Vue.createApp({
               { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' },
             ]
         }
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1
+        },
+        updateImage(variantImage) {
+            this.image = variantImage
+        },
+
+        // Coding Challenge
+        removeFromCart() {
+            this.cart -= 1
+        }
     }
 })
