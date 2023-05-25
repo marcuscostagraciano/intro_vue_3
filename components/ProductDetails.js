cc9f8c1276683c532ff72367f7640ef408ffe5c7
Marcus Vinicius da Costa Graciano <48135737+marcuscostagraciano@users.noreply.github.com>
2023-05-25 20:12:59 -0300

    Add files via upload

---
+++ components/ProductDetails.js
@@ -0,0 +1,15 @@
+// Coding Challenge
+app.component('product-details', {
+    props: {
+        details: {
+            type: Array,
+            required: true
+        }
+    },
+
+    template:
+      /*html*/ 
+      `<ul>
+      <li v-for="detail in details">{{ detail }}</li>
+    </ul>`
+  })
